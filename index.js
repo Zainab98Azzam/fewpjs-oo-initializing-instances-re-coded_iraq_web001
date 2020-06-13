6f09197d846243184a8be49678d962bc3e3ae7a4
Zainab Azzam <zainab_azzam@yahoo.com>
2020-06-13 02:18:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ constructor(salad ,soup,entree,_dessert){
   
 }
 }
-let breakfast = Breakfast("eggs","water")
-let lunch = Lunch("side salad","a","b")
-let dinner =Dinner("balsamic salad")
+let breakfast = new Breakfast("eggs","water")
+let lunch =new Lunch("side salad","a","b")
+let dinner =new Dinner("balsamic salad")
