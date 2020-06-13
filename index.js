5c36517972fec9f2929265df49731c9dbbf04127
Zainab Azzam <zainab_azzam@yahoo.com>
2020-06-13 02:22:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ class Breakfast{
 class Lunch {
   constructor(salad,soup,drink){
     this.salad=salad
-    this soup=soup
-    this.drink
+    this.soup=soup
+    this.drink=drink
   }
 }
 class Dinner{
 constructor(salad ,soup,entree,_dessert){
-  
+  this.salad=salad
+  this.soup=soup
+  this.entree=entree
+  this._dessert=_dessert
 }
 }
 let breakfast = new Breakfast("eggs","water")
